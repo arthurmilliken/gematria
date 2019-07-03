c088aec515b006b994ebb25caf6299959a7bcbb3
Arthur Milliken <amilliken@gmail.com>
2019-07-03 08:25:03 -0700

    .

--- sql/lookup.sql
+++ sql/lookup.sql
@@ -1,17 +1,21 @@
 select
-	i.gematria,
+	max(i.gematria, i.gematria_iota) gematria,
+	w.weight,
 	i.text_original,
 	i.text_plain,
 	i.text_slug,
 	i.pronounce_sbl,
 	i.pronounce_dic,
 	i.strongs,
+	i.text_english,
 	v.ref as verse,
 	v.text,
 	v.verse_id
 from interlinear i
 left join verses v on i.verse_id = v.verse_id and v.edition='kjv'
-where text_slug = 'GPRITh'
+left join weights w on i.word_id = w.word_id
+where text_slug = 'KL-BShR'
+order by weight desc, i.word_id
 ;
 
 select
@@ -27,6 +31,6 @@ select
 	s.derivation
 from word_entries e
 left join strongs s on e.strongs = s.strongs_id
-where gematria = 693
+where gematria = 552
 order by total_weight desc
 ;
